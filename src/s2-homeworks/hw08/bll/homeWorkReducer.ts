7a2d41b8c2fcc00c25df019bf6c404ad40f005ed
polatbek <polatturgunov@gmail.com>
2023-08-29 19:11:05 +0600

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,24 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === "up") {
+                return [...state].sort((a, b) => a.name.localeCompare(b.name)); // need to fix
+            } else if (action.payload === "down") {
+                return [...state].sort((a, b) => b.name.localeCompare(a.name));
+            }
+            return state;
         }
-        case 'check': {
 
-            return state // need to fix
+        case 'check': {
+            if (action.payload >= 18) {
+                return [...state].filter((f) => {
+                    return f.age >= action.payload
+                }) // need to fix
+            }
+            return state
         }
         default:
             return state
