5d21aae9959da51a87243393d70d9a622895984e
polatbek <polatturgunov@gmail.com>
2023-08-29 19:16:54 +0600

    hw8

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -9,8 +9,8 @@ import HW8 from "../../hw08/HW8";
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
-          {/*  <HW6 />
-            <HW7 />*/}
+            <HW6 />
+            <HW7 />
             <HW8 />
             {/*<HW9 />*/}
         </div>
